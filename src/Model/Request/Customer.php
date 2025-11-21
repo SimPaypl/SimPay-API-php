70c987b1961a32a32111f6b2ee39cb10bcd833b0
Patryk Vizauer <vizauerp@gmail.com>
2025-11-21 22:56:31 +0100

    chore: add missing customer data

--- src/Model/Request/Customer.php
+++ src/Model/Request/Customer.php
@@ -6,15 +6,21 @@ namespace Simpay\Model\Request;
 
 class Customer implements RequestInterface
 {
-    private string $name;
-    private string $email;
+    private ?string $name;
+    private ?string $email;
+    private ?string $ip;
+    private ?string $countryCode;
 
     public function __construct(
-        string $name,
-        string $email
+        ?string $name = null,
+        ?string $email = null,
+        ?string $ip = null,
+        ?string $countryCode = null
     ) {
         $this->name = $name;
         $this->email = $email;
+        $this->ip = $ip;
+        $this->countryCode = $countryCode;
     }
 
     public function toArray(): array
@@ -23,6 +29,8 @@ class Customer implements RequestInterface
             'customer' => [
                 'name' => $this->name,
                 'email' => $this->email,
+                'ip' => $this->ip,
+                'countryCode' => $this->countryCode,
             ],
         ];
     }
